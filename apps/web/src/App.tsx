8b5d91423fa95493dd68b52ab881e70939804b03
Davi Reis <dreisss.dev@gmail.com>
2025-06-22 02:06:21 -0300

    feat: add shadcn

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,17 @@
-// import { useState } from "react";
+import { useState } from 'react'
+
+import { Button } from '@/components/ui/button'
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-  return <div>aslkdm</div>
+  return (
+    <div className="grid min-h-screen place-items-center">
+      <Button variant="outline" onClick={() => setCount(count + 1)}>
+        Count: {count}
+      </Button>
+    </div>
+  )
 }
 
 export default App
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3001,
+  },
 })
